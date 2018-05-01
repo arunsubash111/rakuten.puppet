41eaa0a55928efbd78a74e2b709710a0d65eaadb
Arun Subash <arunsubash111@hotmail.com>
2018-05-01 13:19:27 +0530

    Adding release version

--- manifests/rakuten/01.server.pp
+++ manifests/rakuten/01.server.pp
@@ -1,11 +1,43 @@
 class watch_graphql_server {
 
+
+exec { 'yum-update':
+    command => '/usr/bin/yum update bash -y',
+  }
+
+  exec { 'ghost-issue':
+    command => '/usr/bin/yum clean all; /usr/bin/yum update glibc -y',
+  }
+
+  include stdlib
+
+  $source = $::operatingsystemmajrelease ? {
+    7 => 'http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm',
+    6 => 'http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm',
+  }
+
+  package { 'epel-release':
+    ensure          =>  installed,
+    provider        =>  rpm,
+    install_options => ['--nodeps'],
+    source          => $source
+  }
+
+
 class { "nginx":
     worker_processes      => "auto",
     worker_rlimit_nofile  => 64000,
     worker_connections    => 20000,
     server_tokens         => "off"
   }
+
+
+  file { '/aeappdir':
+    ensure => directory,
+    owner   => 'root',
+    group   => 'root',
+  }
+
 
   file { "/webapps/devops":
     ensure => directory,
